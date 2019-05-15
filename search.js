4b65bc3db474ba50581cdb7420ad96684f0931cc
Andrew Miller <ajamesmiller35@gmail.com>
2019-05-14 21:40:56 -0600

    First commit

---
+++ search.js
@@ -0,0 +1,20 @@
+$.fn.highlight = function(){
+
+    $(this).on('keyup',function(){
+
+        console.log($(this).val());
+
+        var valid = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test($(this).val());
+
+        if(valid){
+            $(this).css("background-color", "green");
+            $('#message').replaceWith("<p id='message'>Email is Valid!</p>");
+            //alert('valid');
+        }
+        else{
+            $(this).css("background-color", "red");
+            $('#message').replaceWith("<p id='message'>Email NOT Valid!</p>");
+        }
+    });
+
+};
